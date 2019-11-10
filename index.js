0430a5e3e082026065e54674474414d65d6a1686
jeonghoheo <heojeongho1992@gmail.com>
2019-11-10 21:55:26 +0900

    처음으로 타입스크립트 사용해보기

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+exports.__esModule = true;
+var name = "정호", age = 28, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", you are a " + gender);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "정호",
+  age = 28,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
